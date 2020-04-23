91df009338a3b7b5f701a924de2c19b603249ed3
Dan Dees <dand@appsmiths.com>
2020-04-23 22:14:53 +0700

    Added CMakeLists.txt and README.cmake

---
+++ README.cmake
@@ -0,0 +1,12 @@
+
+supports Windows VS oriented generators
+   Visual Studio 32/64 projects
+   ninja
+   jom
+
+Outputs a DLL - shared library
+
+Unsupported, but not too difficult...
+- Static library as an option
+- Msys/Cygwin
+- Processing *.in config files
